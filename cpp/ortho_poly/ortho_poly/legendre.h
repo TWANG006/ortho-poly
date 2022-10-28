465cb91dcafe178dc10f15a569626665bac5b37a
Tianyi Wang <tianyiwang666@gmail.com>
2022-10-28 16:43:58 -0400

    added comments to legendre

--- cpp/ortho_poly/ortho_poly/legendre.h
+++ cpp/ortho_poly/ortho_poly/legendre.h
@@ -4,27 +4,91 @@
 #include "common.h"
 #include "basepolynomial.h"
 
+//! Class for generate and fit the unassociated Legendre polynomials
+/*! 
+* Details:
+*           1    n   ( n )       n-k        k
+*	Ln(x) = --- Sigma (   )(x - 1)    (x + 1)
+*           2^n  k=0  ( k )
+* 
+* Prom 1D to 2D:
+*	Pj(x,y) = Ln(x)Lm(y)
+* 
+* Recursive equation:
+*	(n + 1)Ln+1(x) = (2n + 1)xLn(x) - nLn-1(x), n >= 1
+*/
 class ORTHOPOLY_API Legendre : public BasePolynomial
 {
 public:
+	/*! Only enable the default construction behaviors*/
 	Legendre() = default;
 	Legendre(const Legendre& lg) = default;
 	Legendre& operator=(const Legendre& lg) = default;
-
 	virtual ~Legendre();
 
 public:
-	/*! Normalization to the [-1, 1] unit square*/
-	virtual Legendre& normalize(const MatrixXXd& X, const MatrixXXd& Y) override;
-	virtual Legendre& normalize(const VectorXd& x) override;
+	//! Normalize the 2D coordinates [-1, 1]
+	virtual Legendre& normalize(
+		const MatrixXXd& X,/*!< [in] 2D x coordinates*/
+		const MatrixXXd& Y /*!< [in] 2D y coordinates*/
+	) override;
 
-	/*! 1D methods*/
-	virtual vec_v gen_1d_p(vec_i& orders) override;
-	virtual std::tuple<vec_v, vec_v> gen_1d_p_dp(vec_i& orders) override;
+	//! Normalize the 1D coordinates to [-1, 1]
+	virtual Legendre& normalize(
+		const VectorXd& x/*!< [in] 1D x coordinates*/
+	) override;
 
-	/*! 2D methods*/
-	virtual vec_m gen_2d_p(const set_i& j_orders) override;
-	virtual std::tuple<MatrixXXd, vec_m> gen_2d_p(const map_id& jorder_coeff) override;
+	//! Generate 1d Legendre polynomials with the orders in the `orders`
+	/*!
+	* NOTEs:
+	*   1. Duplicates are accepted in the `orders`.
+	*   2. Must be proceded with either the () operator or the `normalize` to assign
+	*      values to `x`; e.g., lg(x).gen_1d_p(orders), otherwise, either no polynomials
+	*      are generated or the existing `x` is used.
+	* \return: vector of polynomials corresponding to the `orders`
+	*/
+	virtual vec_v gen_1d_p(
+		vec_i& orders/*!< [in] vector of orders*/
+	) override;
+
+	//! Generate 1D Legendre polynomials and their derivatives
+	/*!
+	* NOTEs:
+	*   1. Duplicates are accepted in the `orders`.
+	*   2. Must be proceded with either the () operator or the `normalize` to assign
+	*      values to `x`; e.g., lg(x).gen_1d_p(orders), otherwise, either no polynomials
+	*      are generated or the existing `x` is used.
+	* \return: tuple of <polynomials, polynomial derivatives> corresponding to
+	*          the `orders`
+	*/
+	virtual std::tuple<vec_v, vec_v> gen_1d_p_dp(
+		vec_i& orders/*!< [in] vector of orders*/
+	) override;
+
+	//! Generate 2D Legendre polynomials and their derivatives
+	/*!
+	* NOTEs:
+	*   1. Duplicates are eliminated by default, since they are passed in as
+	*      `set<int>`, which does not allow duplicate entries. This makes sense
+	*      since 1 order only corresponds to 1 coefficient.
+	*   2. Must be proceded with either the () operator or the `normalize` to assign 
+	*      values to `X`, `Y`; e.g., lg(X, Y).gen_2d_p(orders), otherwise, either no 
+	*      polynomials are generated or the existing `x` is used.
+	* \return: vector of polynomials of the `orders`.
+	*/
+	virtual vec_m gen_2d_p(
+		const set_i& j_orders/*!< [in] set of orders*/
+	) override;
+
+	//! Generate 2D surface from the Legendre polynomials with their correspnding coefficients
+	/*! 
+	* Using the `jorder_coeff` map, generate the corresponding Legendre polynomials, which are then
+	* summed up to generate the 2D surface.
+	* \return: tuple of the 2D surface and the underlying Legendre polynomials.
+	*/
+	virtual std::tuple<MatrixXXd, vec_m> gen_2d_p(
+		const map_id& jorder_coeff/*!< [in] map of <j, c> pairs*/
+	) override;
 
 };
 
